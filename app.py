c0f944b6469af112bf12e32f9fc6e600ea04f5ff
charliel29 <charliel29@nycstudents.net>
2025-10-28 09:25:51 -0400

    movie 1

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+
+
+a=0
+for i in data:
+    if 2018 > (data[a]['year']) > 2014 :
+     print (data[a]['title'])
+    a=a+1
+for i in data 
