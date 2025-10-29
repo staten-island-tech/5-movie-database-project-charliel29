0b21b26c6cd17d7cf57256c9223547dc0d0c8f57
charliel29 <charliel29@nycstudents.net>
2025-10-29 09:26:30 -0400

    genre

--- app.py
+++ app.py
@@ -6,9 +6,30 @@ data = json.load(movies)
 
 
 
-a=0
-for i in data:
-    if 2018 > (data[a]['year']) > 2014 :
-     print (data[a]['title'])
-    a=a+1
-for i in data 
+""" a=0 """
+"""  2018 > (data[a]['year']) > 2014 :
+     prinfor i in data:
+    ift (data[a]['title'])
+    a=a+1 """
+""" for i in data:
+    if (data[a]['year']) == 2023 :
+      print (data[a]['title'])
+    a=a+1 """
+
+
+def movie():
+    a=0
+    for i in data:
+        print (data[a]['title'])
+        a=a+1
+    x = input("What movie you looking for")
+    a=0
+    for i in data:
+        if x == (data[a]['title']):
+            print (data[a])
+        a=a+1
+movie()
+
+
+
+def genre():
